1b9f9b15f22df499bf906385c88581b7800129af
Ozniras <ozniras@gmail.com>
2017-01-11 17:23:50 -0500

    First run of migration code

--- census/50_mapTableMigrBorn.sql
+++ census/50_mapTableMigrBorn.sql
@@ -17,11 +17,11 @@ from thiscase
 -- and p5_age >= 12 and p32_activity_last_12_months >= 1 and p32_activity_last_12_months <= 7
 group by wardid;
 
-drop table if exists censuswardlfbornelsewhere;
-create temporary table censuswardlfbornelsewhere as
+drop table if exists censuswardlfMigrBorn;
+create temporary table censuswardlfMigrBorn as
 select wardid, count(wardid) as lf12months
 from thiscase
-and p6_pob != dist
+where p6_pob != dist
 group by wardid;
 
 drop table if exists censuswardempl12months;
@@ -31,6 +31,14 @@ from thiscase
 where p5_age >= 12 and p32_activity_last_12_months >= 1 and p32_activity_last_12_months <= 6
 group by wardid;
 
+drop table if exists censuswardemplMigrBorn;
+create temporary table censuswardemplMigrBorn as
+select wardid, count(wardid) as empl12months
+from thiscase
+where p5_age >= 12 and p32_activity_last_12_months >= 1 and p32_activity_last_12_months <= 6
+and p6_pob != dist
+group by wardid;
+
 drop table if exists censuswardunem12months;
 create temporary table censuswardunem12months as
 select wardid, count(wardid) as unem12months
@@ -38,32 +46,36 @@ from thiscase
 where p5_age >= 12 and p32_activity_last_12_months = 7
 group by wardid;
 
-drop table if exists censuswardLaborUnskilled;
-create table censuswardLaborUnskilled as
-select dist, const, ward, wardid, population, pop12plus, 
- lf7days, lf12months, empl7days, empl12months, unem7days, unem12months
- from censuswardcounts full outer join censuswardcounts12plus 
-using(wardid)
-full outer join censuswardlf7days 
-using(wardid)
-full outer join censuswardlf12months 
-using(wardid)
-full outer join censuswardempl7days
+drop table if exists censuswardunemMigrBorn;
+create temporary table censuswardunemMigrBorn as
+select wardid, count(wardid) as unem12months
+from thiscase
+where p5_age >= 12 and p32_activity_last_12_months = 7
+and p6_pob != dist
+group by wardid;
+
+drop table if exists censuswardMigrBorn;
+create table censuswardMigrBorn as
+select dist, const, ward, wardid, 
+ lf12months, empl12months, unem12months,
+ lfMigrBorn, emplMigrBorn, unemMigrBorn
+ from censuswardlf12months 
+full outer join censuswardlfMigrBorn
 using(wardid)
 full outer join censuswardempl12months
 using(wardid)
-full outer join censuswardunem7days
+full outer join censuswardemplMigrBorn
 using(wardid)
 full outer join censuswardunem12months
 using(wardid)
+full outer join censuswardunemMigrBorn
+using(wardid)
 ;
-drop table if exists censuswardunem7days;
-drop table if exists censuswardempl7days;
 drop table if exists censuswardunem12months;
+drop table if exists censuswardunemMigrBorn;
 drop table if exists censuswardempl12months;
+drop table if exists censuswardemplMigrBorn;
 drop table if exists censuswardlf12months;
-drop table if exists censuswardlf7days;
-drop table if exists censuswardcounts12plus;
-drop table if exists censuswardcounts;
+drop table if exists censuswardlfMigrBorn;
 drop table if exists thiscase;
 

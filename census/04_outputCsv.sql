2a8b09da6d3b67ee1c9974d49ee45f36624efd51
Ozniras <ozniras@gmail.com>
2017-05-03 15:50:19 -0400

    Change 04_outputCsv.sql to point to temporary directory with postgres permissions

--- census/04_outputCsv.sql
+++ census/04_outputCsv.sql
@@ -1,25 +1,25 @@
-copy censuswardlabor1524 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlabor1524.csv' with csv header;
+copy censuswardlabor1524 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlabor1524.csv' with csv header;
 
-copy censuswardlabor2564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlabor2564.csv' with csv header;
+copy censuswardlabor2564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlabor2564.csv' with csv header;
 
-copy censuswardlaboragriculture1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlaboragriculture1564.csv' with csv header;
+copy censuswardlaboragriculture1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlaboragriculture1564.csv' with csv header;
 
-copy censuswardlaborall1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlaborall1564.csv' with csv header;
+copy censuswardlaborall1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlaborall1564.csv' with csv header;
 
-copy censuswardlaborfemale1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlaborfemale1564.csv' with csv header;
+copy censuswardlaborfemale1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlaborfemale1564.csv' with csv header;
 
-copy censuswardlabormale1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlabormale1564.csv' with csv header;
+copy censuswardlabormale1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlabormale1564.csv' with csv header;
 
-copy censuswardlabormanufacture1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlabormanufacture1564.csv' with csv header;
+copy censuswardlabormanufacture1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlabormanufacture1564.csv' with csv header;
 
-copy censuswardlaborotherindustry1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlaborotherindustry1564.csv' with csv header;
+copy censuswardlaborotherindustry1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlaborotherindustry1564.csv' with csv header;
 
-copy censuswardlabormining1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlabormining1564.csv' with csv header;
+copy censuswardlabormining1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlabormining1564.csv' with csv header;
 
-copy censuswardlaborsemiskilled1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlaborsemiskilled1564.csv' with csv header;
+copy censuswardlaborsemiskilled1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlaborsemiskilled1564.csv' with csv header;
 
-copy censuswardlaborskilled1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlaborskilled1564.csv' with csv header;
+copy censuswardlaborskilled1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlaborskilled1564.csv' with csv header;
 
-copy censuswardlaborunskilled1564 to '/home/ram22/dataDrive/dataProjects/povMap/zambiaLFSmapping/3_csvOutput/censuswardlaborunskilled1564.csv' with csv header;
+copy censuswardlaborunskilled1564 to '/home/ram22/dataDrive/dataProjects/postgresql/tempOutput/censuswardlaborunskilled1564.csv' with csv header;
 
 
